03aa3be834a80fc5d0fe9bee08be3385e5f2a58d
RAMYA-S28 <221401072@rajalakshmi.edu.in>
2025-03-01 22:00:30 +0530

    initial commit

---
+++ food-recipe-app/backend/backend/models/Recipe.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  ingredients: [{
+    type: String,
+    required: true
+  }],
+  instructions: {
+    type: String,
+    required: true
+  },
+  cookingTime: {
+    type: Number,
+    required: true
+  },
+  imageUrl: {
+    type: String
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Recipe', recipeSchema); 
